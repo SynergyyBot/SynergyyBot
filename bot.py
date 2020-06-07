d77de10ad0ccc114e67e406faf33c0e979d975a8
Aadi <aadiamazing5@gmail.com>
2020-06-07 14:28:33 -0400

    init bot.py file

---
+++ bot.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands, tasks, timers
+import random
+import datetime
+
+client = commands.Bot(command_prefix='!')
+
+#On Ready Event------------------------------------------
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.online, activity=discord.Game(" !help"))
+    print('AadiTestBot is ready!')
+
+#Bot Token Pairing--------------------------------
+client.run('TOKEN')
