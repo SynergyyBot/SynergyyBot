65b5a66102e89999219a30efb0d96ed1e15cd068
Aadi <aadiamazing5@gmail.com>
2020-06-07 14:47:34 -0400

    updated on_ready event

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ client = commands.Bot(command_prefix='!')
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game(" !help"))
-    print('AadiTestBot is ready!')
+    print('SynergyyBot is ready!')
 
 #Bot Token Pairing--------------------------------
 client.run('TOKEN')
