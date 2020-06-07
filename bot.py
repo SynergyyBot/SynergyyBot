9ff585c5e83ef81b8f7059f061f8daa48b24f544
Aadi <aadiamazing5@gmail.com>
2020-06-07 15:02:29 -0400

    added latency command

--- bot.py
+++ bot.py
@@ -12,5 +12,11 @@ async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game(" !help"))
     print('SynergyyBot is ready!')
 
+#Commands-----------------------------------------
+
+@client.command() #Ping Command
+async def ping(ctx):
+    await ctx.send(f'Pong! My current latency is {round(client.latency*1000)}ms.')
+
 #Bot Token Pairing--------------------------------
 client.run('TOKEN')
