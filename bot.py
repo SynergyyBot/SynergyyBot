fb0db6d46838cd75b1789db3dbe005364d493de2
Aadi <aadiamazing5@gmail.com>
2020-06-09 18:18:47 -0400

    added general error handling

--- bot.py
+++ bot.py
@@ -22,7 +22,9 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         command_not_found = discord.Embed(title="Command Not Found :(", description="Use !help to see the list of commands and how to use them.", colour=discord.Colour.green())
         await ctx.send(content=None, embed=command_not_found)
-
+    else:
+            general_error = discord.Embed(title="Unexcpected Error!", description="Use !help to see the list of commands and how to use them.", colour=discord.Color.green())
+            await ctx.send(content=None, embed=general_error)
 #Commands-----------------------------------------
 
 @client.command() #Ping Command
