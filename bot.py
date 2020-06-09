2d4f4b7c1d886f74c75f6f03e0886cc935d89959
jerry-cheng5 <j.cheng55555@gmail.com>
2020-06-09 19:04:59 -0400

    Finished !meeting and !poll

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ from dateutil.parser import parse
 
 client = commands.Bot(command_prefix='!')
 timescales = ['sec', 'second', 'min', 'minute', 'hour', 'day', 'week', 'month', 'year'] # right now only supports up to week
+unicode_block = ['🇦','🇧','🇨','🇩','🇪','🇫','🇬','🇭','🇮','🇯','🇰','🇱','🇲','🇳','🇴','🇵','🇶','🇷','🇸','🇹','🇺','🇻','🇼','🇽','🇾','🇿']
 
 #On Ready--------------------------------
 
@@ -91,14 +92,62 @@ async def meeting(ctx, *, information):
                 date = d.strftime('%A, %b %-d, %Y')
 
                 #Create embed
-                meeting_card = discord.Embed(title=f"\U0001F5D3 Meeting Created: {name}", name="Meeting Time", value=f"{time} on {date}", colour=discord.Colour.green())
+                meeting_card = discord.Embed(title=f"\U0001F5D3 Meeting Created: {name}", colour=discord.Colour.green())
+                meeting_card.add_field(name="Meeting Time", value=f"{time} on {date}")
                 await ctx.send(content=None, embed=meeting_card)
             else:
                 await ctx.send(content=None, embed=time_missing)
         else:
             await ctx.send(content=None, embed=format_error)
 
+@client.command()
+async def poll(ctx, *, information):
+    if '\"' in information and ',' in information and information[len(information)-1].isnumeric():
+        op = []
+        polltimeinminutes = 0
+        title = information[information.index('\"')+1 : information.rindex('\"')]
+        temp = information.rindex('\"')
+        for i in range(information.rindex('\"')+1, len(information)):
+            if information[i] == ',':
+                op.append(information[temp+1:i].strip())
+                temp = i
+        for j in reversed(information):
+            if not j.isnumeric():
+                polltimeinminutes = int(information[information.rindex(j)+1:].strip())
+                break
+        op.append(information[temp+1 : information.rindex(str(polltimeinminutes))].strip())
 
+        if len(op) <= 26:
+
+            options = {}
+            for i in range(len(op)):
+                options[unicode_block[i]] = op[i]
+            
+            vote = discord.Embed(title=f"\U0001F4F6 {title}", color=discord.Colour.green()) 
+            value = "\n".join("{} - {}".format(*item) for item in options.items())
+            vote.add_field(name="Options:", value=value, inline=False) 
+            vote.set_footer(text="Time to vote: %s minute.\nUse the reactions below to vote." % (polltimeinminutes))
+
+            message_1 = await ctx.send(embed=vote)
+            for choice in options:
+                await message_1.add_reaction(emoji=choice)
+
+            polltimeinminutes *= 60
+            await asyncio.sleep(polltimeinminutes)
+            message_1 = await ctx.fetch_message(message_1.id)
+
+            counts = {react.emoji: react.count for react in message_1.reactions}
+            winner = max(options, key=counts.get)
+
+            winner_card = discord.Embed(color=discord.Colour.green(), description= "\U00002B50 The winner of the poll **%s** is **%s**!" % (title, options[winner]))
+
+            await ctx.send(embed=winner_card)
+        
+        else:
+            too_many_options = discord.Embed(title="Too many options!", description="The limit for !poll is 26", color=discord.Colour.green())
+            await ctx.send(embed=too_many_options)
+        
+    
 #Other Functions--------------------------
 
 def has_date(string, fuzzy=True):
@@ -129,4 +178,4 @@ async def meeting_error(ctx, error):
 
 
 #Client Token (removed for security reasons)
-client.run('NzE3NDMyMzc0OTY2ODc4MjM4.Xt_qqA.vMqd06AhGLI-xa8H3iaDGX5k7Tk')
+client.run('TOKEN')
